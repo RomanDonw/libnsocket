0e28e112a6a113bf7288fe50519c8c12b20752b3
RomanDonw <romandonw628@gmail.com>
2026-05-10 14:49:34 +1000

    updated CMake

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,7 +1,7 @@
 macro(test name sources)
     add_executable(${name} ${sources} "tests/base/base.c")
     target_include_directories(${name} PRIVATE "tests/base")
-    target_link_libraries(${name} PRIVATE socket)
+    target_link_libraries(${name} PRIVATE ${PROJECT_NAME})
 endmacro()
 
 test(test0 "tests/0.c")
